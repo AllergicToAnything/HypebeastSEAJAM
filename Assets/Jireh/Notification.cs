129e28586f80b8f45c7521a5c2b82fcfe8166981
Jireh <jireh_cwe@hotmail.com>
2019-11-02 16:01:02 +0800

    Dragging notification ok

--- Assets/Jireh/Notification.cs
+++ Assets/Jireh/Notification.cs
@@ -8,6 +8,12 @@ public class Notification : MonoBehaviour
     private Camera mainCam;
     private bool isOverNotification;
 
+    public Vector3 mousePreviousLocation;
+    public Vector3 velocity;
+    public Vector3 objectCurrentPosition;
+    public Vector3 objectTargetPosition;
+    public float topSpeed = 10;
+
     void Start()
     {
         mainCam = Camera.main;
@@ -18,19 +24,41 @@ public class Notification : MonoBehaviour
         
     }
 
-    private void OnMouseDown()
-    {
-
-        // Vector3 mousePos = mainCam.ScreenPointToRay();
+     void OnMouseDown() {
+        //This grabs the position of the object in the world and turns it into the position on the screen
+        print("hit");
+        //Sets the mouse pointers vector3
+        mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
     }
 
-    private void OnMouseDrag()
-    {
-        
-    }
+    void OnMouseDrag() {
+        Vector3 mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 screenToWorldPoint = mainCam.ScreenToWorldPoint(mouseCurLocation);
+        Vector3 screenToWorldPointFixed = new Vector3(screenToWorldPoint.x, screenToWorldPoint.y, 0);
+        transform.position = Vector3.Lerp(transform.position, screenToWorldPointFixed, 0.5f);
 
-    private void OnMouseUp()
-    {
-        
+
+        velocity = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
+        mousePreviousLocation = mouseCurLocation;
+    }
+ 
+    public void OnMouseUp() {
+    //Makes sure there isn't a ludicrous speed
+        Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
+        if (rb2D == null){
+            gameObject.AddComponent(typeof(Rigidbody2D));
+            rb2D = GetComponent<Rigidbody2D>(); 
+        } 
+
+        rb2D.bodyType = RigidbodyType2D.Dynamic;
+        print(velocity);
+        // GetComponent<Rigidbody2D>().velocity = velocity;
+        gameObject.transform.right = velocity;
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(velocity.x, velocity.y) + new Vector2(0, -5));
+        if (velocity.magnitude > topSpeed){
+            velocity = GetComponent<Rigidbody2D>().velocity.normalized * topSpeed;
+        }
     }
 }
--- Assets/Jireh/NotificationSpawner.cs
+++ Assets/Jireh/NotificationSpawner.cs
@@ -10,48 +10,27 @@ public class NotificationSpawner : MonoBehaviour
     
     public GameObject pointer;
 
+
+
+
+    private Camera mainCam;
+
     void Start () {
-         
-     }
+        mainCam = Camera.main;
+    }
  
      // Update is called once per frame
     void Update () {
      //This prevents your thrown object from ascending to infinity and beyond. Disable if you're trying to throw Buzz Lightyear.
-        if (force.y > 0.0f) {
-            force.y -= 0.1f;
-            }
+        // if (force.y > 0.0f) {
+        //     force.y -= 0.1f;
+        // }
+        Vector3 screenToWorldPoint = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        Vector3 screenToWorldPointFixed = new Vector3(screenToWorldPoint.x, screenToWorldPoint.y, 0);
+        pointer.transform.position = screenToWorldPointFixed;
     }
 
 
-    public Vector3 gameObjectSreenPoint;
-    public Vector3 mousePreviousLocation;
-    public Vector3 mouseCurLocation;
-
-    void OnMouseDown() {
-        //This grabs the position of the object in the world and turns it into the position on the screen
-        gameObjectSreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        //Sets the mouse pointers vector3
-        mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
-    }
- 
-    public Vector3 force;
-    public Vector3 objectCurrentPosition;
-    public Vector3 objectTargetPosition;
-    public float topSpeed = 10;
-    void OnMouseDrag() {
-        mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
-        force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
-        mousePreviousLocation = mouseCurLocation;
-    }
- 
-    public void OnMouseUp() {
-    //Makes sure there isn't a ludicrous speed
-    if (GetComponent<Rigidbody>().velocity.magnitude > topSpeed)
-        force = GetComponent<Rigidbody>().velocity.normalized * topSpeed;
-    }
- 
-    public void FixedUpdate() {
-        GetComponent<Rigidbody>().velocity = force;
-    }
+   
 
 }
