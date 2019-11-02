1d885c933315f4123e8cf607b25d982c786fd8a6
DarkRavenStar <123teenakannan@gmail.com>
2019-11-02 16:07:32 +0800

    Enemy System
    Enemy System

---
+++ Assets/Teena/BaseEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    public Vector3 enemyPos;
+    public EnemyData enemyData;
+
+
+    public GameObject target;
+    public Transform targetPosTransform;
+    public Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Teena/SlowIconEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowIconEnemy : BaseEnemy
+{
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Spin object around Y-Axis
+        //transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        float step = enemyData.speed * Time.deltaTime; // calculate distance to move
+        tempPos = Vector3.MoveTowards(transform.position, targetPos, step);
+
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+
+        targetPos = target.transform.position;
+        
+    }
+}
