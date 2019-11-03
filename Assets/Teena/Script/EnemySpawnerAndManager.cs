0b9d401affb5eb1d4fc8c100dcb40901856dca53
DarkRavenStar <123teenakannan@gmail.com>
2019-11-03 08:50:51 +0800

    Enemy SpawnerUpdate

--- Assets/Teena/Script/EnemySpawnerAndManager.cs
+++ Assets/Teena/Script/EnemySpawnerAndManager.cs
@@ -2,35 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class SpawnWave
+{
+    public EnemyListData enemyList;
+    public float SpawnTimer;
+    public int EnemyPerCycle;
+    public int NumOfCycles;
+}
+
 public class EnemySpawnerAndManager : MonoBehaviour
 {
     BaseEnemy[] SpawnedEnemies;
+    public SpawnWave[] spawnWaves;
     public IconSpriteListData iconSpriteList;
     public EnemyListData enemyList;
     public GameObject target;
     public float SpawnTimer;
     float defSpawnTimer;
-    public bool AllowSpawn;
+    bool AllowSpawn;
     public BoxCollider2D SpawnArea;
     public int EnemyPerCycle;
+    public int NumOfCycles;
+    int CycleCounter;
+    public EnemySpawnerAndManager[] managersToObserve;
+    public bool SpawnBasedOnObservedFinished;
+    public bool FinishedCycle = false;
+    public bool FinishedLocalCycle = false;
+    public int FinalWaveInt = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        defSpawnTimer = SpawnTimer;
+        SetWave(0);
+    }
+
+    void SetWave(int waveNum)
+    {
+        if(waveNum < spawnWaves.Length)
+        {
+            defSpawnTimer = spawnWaves[waveNum].SpawnTimer;
+            EnemyPerCycle = spawnWaves[waveNum].EnemyPerCycle;
+            NumOfCycles = spawnWaves[waveNum].NumOfCycles;
+            enemyList = spawnWaves[waveNum].enemyList;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpawnTimer -= Time.deltaTime;
-        if(SpawnTimer <= 0.0f)
+        if(FinishedCycle == false)
         {
-            for(int i = 0; i < EnemyPerCycle; i++)
+            if (SpawnBasedOnObservedFinished == true)
             {
-                SpawnEnemy();
+                if(CheckObserved() == true)
+                {
+                    AllowSpawn = true;
+                }
             }
-            SpawnTimer = defSpawnTimer;
+            else
+            {
+                AllowSpawn = true;
+            }
+
+            if(AllowSpawn == true)
+            {
+                SpawnTimer -= Time.deltaTime;
+                if (SpawnTimer <= 0.0f)
+                {
+                    for (int i = 0; i < EnemyPerCycle; i++)
+                    {
+                        SpawnEnemy();
+                    }
+                    SpawnTimer = defSpawnTimer;
+                    CycleCounter++;
+                    if (CycleCounter >= NumOfCycles)
+                    {
+                        FinishedCycle = true;
+                    }
+                }
+            }
         }
     }
 
@@ -51,5 +102,18 @@ public class EnemySpawnerAndManager : MonoBehaviour
         Sprite spawnedSprite = iconSpriteList.GetRandomSprite();
         BaseEnemy spawnedEnemy = (BaseEnemy)Instantiate(enemyObject, spawnPosition, Quaternion.identity);
         spawnedEnemy.sprite = spawnedSprite;
+    }
+
+    bool CheckObserved()
+    {
+        for(int i = 0; i < managersToObserve.Length; i++)
+        {
+            if(managersToObserve[i].FinishedCycle == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
